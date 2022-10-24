a21cfb5e8ac53286d1b6c2c5133b763f840ae1b6
Aashutosh Soni <ashutoshsoni130@gmail.com>
2022-10-24 23:53:09 +0530

    new file:   python problems/takeFristNameLastnameandreverse.py

---
+++ python problems/takeFristNameLastnameandreverse.py
@@ -0,0 +1,4 @@
+firstName = input("Enter your first Name- ");
+secondName = input("Enter your second Name- ");
+print("Name Before swap= ",firstName,secondName);
+print("Name After swap= ",secondName,firstName);
