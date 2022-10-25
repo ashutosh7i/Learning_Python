82c0663e8b98ae9b69dc2e99b3538ae6a01b578b
Aashutosh Soni <ashutoshsoni130@gmail.com>
2022-10-25 20:33:54 +0530

    new file:   functions.py

---
+++ functions.py
@@ -0,0 +1,16 @@
+#define functionname(parameters): { body}
+def sayHello():
+    print("Hello!");
+
+
+#using parameters
+def sayThis(textToSay):
+    print(textToSay);
+
+def addition(num1,num2):
+    print(num1+num2)
+
+    
+def main():
+    addition(3,2)
+
