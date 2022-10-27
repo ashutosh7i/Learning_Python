f161e118f498206daa6a7ca1163e1a2cb0413a58
Aashutosh Soni <ashutoshsoni130@gmail.com>
2022-10-27 12:41:05 +0530

    functions with default Parameters

---
+++ FunctionWDefaultParameters.py
@@ -0,0 +1,11 @@
+#we can have default parameters in a functions 
+def testFunction(name="ashutosh",age=18):
+    print("Name= ",name);
+    print("age= ",age);
+
+
+#when called with some parameter-
+testFunction("temp",9);
+
+#when called without any  parameter-
+testFunction();
